33d036bf2d95dd330497821149acff53663f84cd
Patrik Stenmark <patrik.stenmark@gmail.com>
2009-06-16 19:49:43 +0200

    Gjort eventsteps som tar table med attribut som argument

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -1,6 +1,30 @@
-Given /^an event "([^\"]*)"$/ do |arg1|
-  Factory(:event)
+Given /^an event "([^\"]*)" with fields:$/ do |name, table|
+  event = Factory(:event, :name => name)
+
+  unless table.raw.empty?
+    table.hashes.each do |field|
+      puts field.inspect
+      puts field["Name"].inspect
+      case field["Name"]
+      when /deadline/
+        field['Value'] =~ /(\d)+ days ago/
+        deadline = $1.to_i.days.ago
+        event.deadline = deadline
+        
+      else
+        event.send("#{field['Name']}=", field['Value'])
+      end
+    end
+  end
+  
+  event.save!
+  
 end
+
+Given /^an event "([^\"]*)"$/ do |name|
+  Given %Q{an event "#{name}" with fields:}, Cucumber::Ast::Table.new([])
+end
+
 
 #Given /^a guest to "([^\"]*)" called "([^\"]*)"$/ do |event_name, name|
 #  event = Event.find_by_name(event_name)
