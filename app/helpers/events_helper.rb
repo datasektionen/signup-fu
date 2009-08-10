2773125b500e2949348608059cdbeca24fc40330
Patrik Stenmark <patrik.stenmark@gmail.com>
2009-08-10 14:57:46 +0200

    refactoring if till case

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,16 @@ module EventsHelper
   def food_preferences(reply)
     reply.food_preferences.map(&:name).join(", ") 
   end
+  
+  def state(reply)
+    case reply.aasm_current_state
+    when :expired
+      "Expired (No payment)"
+    when :paid
+      "Paid (#{reply.paid_at.strftime("%Y-%m-%d")})"
+    when :new
+      "Unpaid"
+    end
+    
+  end
 end
