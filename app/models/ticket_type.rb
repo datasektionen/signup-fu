afb1463c07b4f642c6425cdfd653efaad3665163
Patrik Stenmark <spatrik@wpa-n2-77.kthopen.kth.se>
2009-10-14 14:17:27 +0200

    tillåt 0-pris

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -3,5 +3,5 @@ class TicketType < ActiveRecord::Base
   
   validates_uniqueness_of :name, :scope => :event_id
   
-  validates_numericality_of :price, :only_integer => true, :greater_than => 0
+  validates_numericality_of :price, :only_integer => true, :greater_than_or_equal_to => 0
 end
