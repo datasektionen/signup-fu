8c37d98e7a8a6b7ffc60816c9be547c0a45ae022
Patrik Stenmark <spatrik@patrik-stenmarks-macbook-pro.local>
2009-10-22 19:48:16 +0200

    Förhoppningsvis fått compass att funka på heroku

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,9 @@
 require 'compass'
-# If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
-Compass.configure_sass_plugin!
+if ENV['HEROKU_TYPE'].nil?
+  # If you have any compass plugins, require them here.
+  Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+  Compass.configuration.environment = RAILS_ENV.to_sym
+  Compass.configure_sass_plugin!
+else
+  Sass::Plugin.options[:never_update] = true
+end
