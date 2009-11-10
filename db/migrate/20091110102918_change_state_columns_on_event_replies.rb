c9b3c1a934a8262f4f4511b1dd37cb5ca0206071
patrik.stenmark@gmail.com <spatrik@patrik-stenmarks-macbook-pro.local>
2009-11-10 12:15:37 +0100

    Fixat migration så den uppdaterar gammal data.

--- db/migrate/20091110102918_change_state_columns_on_event_replies.rb
+++ db/migrate/20091110102918_change_state_columns_on_event_replies.rb
@@ -1,9 +1,34 @@
 class ChangeStateColumnsOnEventReplies < ActiveRecord::Migration
   def self.up
+    
+    aasm_states = EventReply.all.to_a.inject({}) do |hash, reply|
+      hash[reply.id] = reply.aasm_state
+      hash
+    end
+    
     change_table :event_replies do |t|
       t.remove :aasm_state
       t.string :payment_state
       t.string :guest_state
+    end
+    
+    EventReply.reset_column_information 
+    
+    EventReply.all.each do |reply|
+      (puts "invalid event: #{reply.event_id}" && next) if reply.event.nil?
+      
+      aasm_state = aasm_states[reply.id]
+      if aasm_state == "cancelled"
+        reply.payment_state = "new"
+        reply.guest_state = "cancelled"
+      elsif aasm_state == "new"
+        reply.payment_state = "new"
+        reply.guest_state = "new"
+      else
+        puts "UNKNOWN STATE: #{aasm_state}. ID: #{reply.id}"
+        next
+      end
+      reply.save!
     end
   end
 
