5ac393346d17d69970734eed779c36a1b9bea4d7
Patrik Stenmark <patrik.stenmark@gmail.com>
2009-12-14 17:19:35 +0100

    migration-fix

--- db/migrate/20091213182220_make_event_act_as_authentic.rb
+++ db/migrate/20091213182220_make_event_act_as_authentic.rb
@@ -2,6 +2,18 @@ class MakeEventActAsAuthentic < ActiveRecord::Migration
   def self.up
     add_column :events, :encrypted_password, :string, :null => false, :default => ""
     add_column :events, :persistence_token, :string
+    
+    Event.reset_column_information 
+    
+    Event.all.each do |event|
+      begin
+        event.password = event.password_confirmation = 'kaka1234'
+        event.save!
+      rescue => e
+        puts "#{event.name} (#{event.id}) is defective:"
+        puts e.message
+      end
+    end
   end
 
   def self.down
