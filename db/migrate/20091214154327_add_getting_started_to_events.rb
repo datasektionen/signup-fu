c7445611e28be4f9fe0b672d20aa6f3efe4aac02
Patrik Stenmark <patrik.stenmark@gmail.com>
2009-12-16 17:50:06 +0100

    eh stupid me

--- db/migrate/20091214154327_add_getting_started_to_events.rb
+++ db/migrate/20091214154327_add_getting_started_to_events.rb
@@ -1,6 +1,6 @@
 class AddGettingStartedToEvents < ActiveRecord::Migration
   def self.up
-    add_column :events, :getting_started_dismissed, :boolean, :null => false, :default => 0
+    add_column :events, :getting_started_dismissed, :boolean, :null => false, :default => false
   end
 
   def self.down
