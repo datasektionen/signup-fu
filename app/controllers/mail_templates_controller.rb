dd7fc64d50a1c46ccda48f1d291b48153f53ee75
Patrik Stenmark <patrik.stenmark@gmail.com>
2009-12-19 20:47:40 +0100

    Bort med onödiga filer

--- app/controllers/mail_templates_controller.rb
+++
@@ -1,8 +0,0 @@
-class MailTemplatesController < ApplicationController
-  before_filter :load_parent, :only => [:new, :create]
-  private
-  
-  def load_parent
-    @event = Event.find(params[:event_id])
-  end
-end
