fa45a06abdbf5719a8040f394e1beb1aef1d8700
Patrik Stenmark <patrik.stenmark@gmail.com>
2009-12-21 17:29:20 +0100

    dj cap tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,3 +36,23 @@ namespace :deploy do
   end
   after :"deploy:symlink", :"deploy:copy_config"
 end
+
+namespace :delayed_job do
+  desc "Start delayed_job process"
+  task :start, :roles => :app do
+    run "cd #{current_path}; script/delayed_job start"
+  end
+ 
+  desc "Stop delayed_job process"
+  task :stop, :roles => :app do
+    run "cd #{current_path}; script/delayed_job stop"
+  end
+ 
+  desc "Restart delayed_job process"
+  task :restart, :roles => :app do
+    run "cd #{current_path}; script/delayed_job restart"
+  end
+end
+after "deploy:start", "delayed_job:start"
+after "deploy:stop", "delayed_job:stop"
+after "deploy:restart", "delayed_job:restart"
