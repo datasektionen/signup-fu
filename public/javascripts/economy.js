b7b955f83f6a201801e46bd60e874f1ddece1d2a
Patrik Stenmark <patrik.stenmark@gmail.com>
2009-12-23 23:15:39 +0100

    Javascriptat ekonomitabellen

---
+++ public/javascripts/economy.js
@@ -0,0 +1,35 @@
+document.observe("dom:loaded", function(event) {
+  $("economy_report_table").observe("click", function(event) {
+    var clickedRow = event.findElement('tr');
+    if (clickedRow) {
+      toggleCheckbox("reply_" + clickedRow.id.sub("reply_row_", ""));
+    }
+  });
+  
+  $('economy_report_table').observe("mouseover", function(event) {
+    var row = event.findElement('tr');
+    if (row) {
+      row.addClassName("highlight");
+    }
+  });
+  
+  $('economy_report_table').observe("mouseout", function(event) {
+    var row = event.findElement('tr');
+    if (row) {
+      row.removeClassName("highlight");
+    }
+  });
+  
+});
+
+function toggleCheckbox(checkbox) {
+  checkbox = $(checkbox);
+  if (checkbox == null) {
+    return;
+  }
+  checkbox.checked = !checkbox.checked;
+}
+
+function toggleFoo(row) {
+  toggleCheckbox("reply_" + row.id.sub("reply_row_", ""));
+}
