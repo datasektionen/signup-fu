008a2d0c9bd6ae4ae74619e1b75f467c5b327708
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-01-02 16:59:45 +0100

    login-path

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resource :user_session
   
+  map.login "login", :controller => 'user_sessions', :action => 'new'
+  
   map.root :controller => 'home', :action => 'index'
 end
