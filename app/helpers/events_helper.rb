ab38dc1a178be26b7f2a2c623e115c293db8229e
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-01-03 21:42:27 +0100

    i18nized events_helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,25 +6,25 @@ module EventsHelper
   def payment_state(reply)
     case reply.payment_state_name
     when :expired
-      "Expired (No payment)"
+      t('event_replies.states.payment.expired')
     when :paid
-      "Paid (#{reply.paid_at.strftime("%Y-%m-%d")})"
+      t('event_replies.states.payment.paid', :at => reply.paid_at.strftime("%Y-%m-%d"))
     when :new
-      "Unpaid"
+      t('event_replies.states.payment.new')
     when :reminded
-      "Reminded (#{reply.reminded_at.strftime("%Y-%m-%d")})"
+      #"Reminded (#{reply.reminded_at.strftime("%Y-%m-%d")})"
+      t('event_replies.states.payment.reminded')
     else
       "Unknown state"
     end
-    
   end
   
   def guest_state(reply)
     case reply.guest_state_name
     when :unknown
-      "New"
+      t('event_replies.states.guest.new')
     when :cancelled
-      "Cancelled"
+      t('event_replies.states.guest.cancelled')
     when :attending
       "Attending"
     else
