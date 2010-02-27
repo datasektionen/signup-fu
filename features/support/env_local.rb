546de4cc26c017a9e3653034f9d0f26ab4fcfce8
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-02-27 20:03:39 +0100

    webrat and cucumber upgrade of doom

---
+++ features/support/env_local.rb
@@ -0,0 +1,16 @@
+require "authlogic/test_case"
+require 'email_spec/cucumber'
+
+require "spec/mocks"
+
+Before do
+  $rspec_mocks ||= Spec::Mocks::Space.new
+end
+
+After do
+  begin
+    $rspec_mocks.verify_all
+  ensure
+    $rspec_mocks.reset_all
+  end
+end
