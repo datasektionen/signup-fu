7cabc1ad52a548bee29de020212d7f17df99663e
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-10-16 21:54:42 +0200

    STUPID SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ SignupFu::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
 end
