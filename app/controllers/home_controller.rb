fa085b15573ff1c70d3ca73ea84c45553fb3a627
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-11-04 20:32:12 +0100

    styr upp layouter och lite texter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!
   layout 'start'
   def index
-    if current_user.present?
-      redirect_to events_path
-    end
+
   end
 end
