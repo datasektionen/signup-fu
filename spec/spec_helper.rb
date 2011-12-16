c02d8a2045c77fa758e15a71f7078167bdf8a759
Victor Koronen <koronen@kth.se>
2011-12-16 15:13:07 +0100

    Don't fail fast per default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,8 +29,6 @@ RSpec.configure do |config|
 
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
-  config.fail_fast = true
-
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
   end
