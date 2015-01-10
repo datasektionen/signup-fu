a5f5b9a51ee0ce8833c706210845776711ffa05e
Your Name <patrik@stenmark.io>
2015-01-10 12:27:05 +0100

    Fix db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-User.create!(:email => 'patrik.stenmark@gmail.com', :password => 'kaka123')
+User.create!(email: 'frasse@example.com', password: 'kaka123', name: "frasse-festfixare")
